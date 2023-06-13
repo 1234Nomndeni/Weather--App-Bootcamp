e28bf32a1e622bfaad64031e3d7309e61044d486
1234Nomndeni <Nomdeni.Nkosi@gmail.com>
2023-06-14 01:25:04 +0300

    Done with interface

---
+++ Weatherapp.js
@@ -0,0 +1,15 @@
+function greet (){
+    alert("Good evening PLP web developers")
+}
+
+let inputValuue = document.querySelector(".search-box")
+let button = document.querySelector(".search-button")
+let city = document.querySelector(".location .city")
+let date = document.querySelector(".location .date")
+let temp = document.querySelector(".current .temp")
+let description = document.querySelector(".current .weather")
+
+button.addEventListener("click", function(){
+ fetch()
+})
+
